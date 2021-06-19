2970c5d3785a36b083411f8931bbb7aab4d36342
tokizuoh <k_sakai@yumemi.co.jp>
2021-06-19 22:33:52 +0900

    add processing fetch file on firebase-storage

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/tokizuoh/dianthus-server
+
+go 1.15
+
+require (
+	cloud.google.com/go/firestore v1.5.0 // indirect
+	cloud.google.com/go/storage v1.15.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210615190721-d04028783cf1 // indirect
+	google.golang.org/api v0.48.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "log"
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+)
 
 func main() {
-	log.Println("Hello, World!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	storageBucket := os.Getenv("FIREBASE_STORAGE_BUCKET")
+
+	config := &firebase.Config{
+		StorageBucket: storageBucket,
+	}
+	cred := os.Getenv("FIREBASE_CREDENTIAL_FILE_PATH")
+	opt := option.WithCredentialsFile(cred)
+	app, err := firebase.NewApp(context.Background(), config, opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := app.Storage(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bucket, err := client.DefaultBucket()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fp := os.Getenv("FIREBASE_STORAGEL_FILE_PATH")
+	ctx := context.Background()
+	rc, err := bucket.Object(fp).NewReader(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rc.Close()
+
+	data, err := ioutil.ReadAll(rc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(data))
 }
